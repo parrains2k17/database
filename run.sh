07eea3ca6aa55b0d0a553c1f4e2581b652b7a537
Guillaume Wuip <guillaume@wuips.com>
2017-03-09 12:51:12 +0100

    Mount directory for persistent storage

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 
+mkdir -p data
 docker run \
     --name parrains-db \
-    --env-file .env
-    -p 3306:3306
+    --env-file .env \
+    -p 3306:3306 \
+    -v $(pwd)/data:/var/lib/mysql \
     -d mysql:8.0.0
