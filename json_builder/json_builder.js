fde0267bdf882e349432d2c9bd4f5d3531e330cf
Arnaud Langelier <arnaud.langelier@etu.univ-nantes.fr>
2017-03-15 11:36:02 +0100

    test foreach + stringify

--- json_builder/json_builder.js
+++ json_builder/json_builder.js
@@ -34,6 +34,7 @@ var candidats = [
 					"YADE Rama"
 				]
 
+
 connection.connect(function(err) {
 	if (err) {
 		console.error('error connecting : ' + err.stack);
@@ -45,15 +46,19 @@ connection.connect(function(err) {
 });
 
 
-connection.query({
-  sql: 'SELECT * FROM `parrainages_enrichis` WHERE `nom` = ?',
-  timeout: 3600, // 3.600s :-) 
-  values: ['DUPONT']
-}, function (error, results, fields) {
-  // error will be an Error if one occurred during the query 
-  // results will contain the results of the query 
-  // fields will contain information about the returned results fields (if any) 
-  console.log('results : '+ JSON.stringify(results));
+candidats.forEach(function(candidat){
+
+	connection.query({
+
+  		sql: 'SELECT * FROM `parrainages_enrichis` WHERE `candidat` = ?',
+  		timeout: 3600, // 3.600s :-) 
+  		values: candidat
+	}, function (error, results, fields) {
+  		// error will be an Error if one occurred during the query 
+  		// results will contain the results of the query 
+  		// fields will contain information about the returned results fields (if any) 
+  		console.log('results : '+ JSON.stringify(results));
+	});
 });
 
 
