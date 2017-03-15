6badd2a968ad25fe6b4f40d566001fdfe0bbc35f
Arnaud Langelier <arnaud.langelier@etu.univ-nantes.fr>
2017-03-15 11:28:34 +0100

    ajout tableau candidats

--- json_builder/json_builder.js
+++ json_builder/json_builder.js
@@ -10,6 +10,30 @@ var connection  = mysql.createConnection({
 	database	: process.env.MYSQL_DATABASE
 });
 
+var candidats = [
+					"ALLIOT-MARIE Michèle", 
+					"ARTHAUD Nathalie", 
+					"ASSELINEAU François", 
+					"CHEMINADE Jacques", 
+					"DUPONT-AIGNAN Nicolas",
+					"FAUDOT Bastien",
+					"FILLON François",
+					"GORGES Jean-Pierre",
+					"GUAINO Henri",
+					"HAMON Benoit",
+					"JARDIN Alexandre",
+					"LARROUTUROU Pierre",
+					"LASSALLE Jean",
+					"LE PEN Marine",
+					"MACRON Emmanuel",
+					"MARCHANDISE Charlotte",
+					"MELENCHON Jean-Luc",
+					"POUTOU Philippe",
+					"TAUZIN Didier",
+					"TEMARU Oscar",
+					"YADE Rama"
+				]
+
 connection.connect(function(err) {
 	if (err) {
 		console.error('error connecting : ' + err.stack);
@@ -20,15 +44,16 @@ connection.connect(function(err) {
 
 });
 
+
 connection.query({
   sql: 'SELECT * FROM `parrainages_enrichis` WHERE `nom` = ?',
-  timeout: 3600000, // 3600s :-) 
+  timeout: 3600, // 3.600s :-) 
   values: ['DUPONT']
 }, function (error, results, fields) {
   // error will be an Error if one occurred during the query 
   // results will contain the results of the query 
   // fields will contain information about the returned results fields (if any) 
-  console.log('results : '+ results);
+  console.log('results : '+ JSON.stringify(results));
 });
 
 
