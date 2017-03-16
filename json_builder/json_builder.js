0c1007630c07e4e2454e831c3bb7ece8b22d59c7
Arnaud Langelier <arnaud.langelier@etu.univ-nantes.fr>
2017-03-16 11:37:57 +0100

    solved asynchronous problems + better json generated

--- json_builder/json_builder.js
+++ json_builder/json_builder.js
@@ -1,68 +1,101 @@
-var mysql		= require('mysql');
-var dotenv		= require('dotenv');
+const mysql       = require('mysql');
+const dotenv      = require('dotenv');
+const fs          = require('fs');
 dotenv.config({path:'../.env'});
 
-var connection  = mysql.createConnection({
-	host		: process.env.MYSQL_HOST,
-	port 		: process.env.MYSQL_PORT,
-	user        : process.env.MYSQL_ID,
-	password	: process.env.MYSQL_PASSWORD,
-	database	: process.env.MYSQL_DATABASE
+const connection  = mysql.createConnection({
+    host        : process.env.MYSQL_HOST,
+    port        : process.env.MYSQL_PORT,
+    user        : process.env.MYSQL_ID,
+    password    : process.env.MYSQL_PASSWORD,
+    database    : process.env.MYSQL_DATABASE
 });
 
-var candidats = [
-					"ALLIOT-MARIE Michèle", 
-					"ARTHAUD Nathalie", 
-					"ASSELINEAU François", 
-					"CHEMINADE Jacques", 
-					"DUPONT-AIGNAN Nicolas",
-					"FAUDOT Bastien",
-					"FILLON François",
-					"GORGES Jean-Pierre",
-					"GUAINO Henri",
-					"HAMON Benoit",
-					"JARDIN Alexandre",
-					"LARROUTUROU Pierre",
-					"LASSALLE Jean",
-					"LE PEN Marine",
-					"MACRON Emmanuel",
-					"MARCHANDISE Charlotte",
-					"MELENCHON Jean-Luc",
-					"POUTOU Philippe",
-					"TAUZIN Didier",
-					"TEMARU Oscar",
-					"YADE Rama"
-				]
+const candidats = [
+                    "ALLIOT-MARIE Michèle", 
+                    "ARTHAUD Nathalie", 
+                    "ASSELINEAU François", 
+                    "CHEMINADE Jacques", 
+                    "DUPONT-AIGNAN Nicolas",
+                    "FAUDOT Bastien",
+                    "FILLON François",
+                    "GORGES Jean-Pierre",
+                    "GUAINO Henri",
+                    "HAMON Benoit",
+                    "JARDIN Alexandre",
+                    "LARROUTUROU Pierre",
+                    "LASSALLE Jean",
+                    "LE PEN Marine",
+                    "MACRON Emmanuel",
+                    "MARCHANDISE Charlotte",
+                    "MELENCHON Jean-Luc",
+                    "POUTOU Philippe",
+                    "TAUZIN Didier",
+                    "TEMARU Oscar",
+                    "YADE Rama"
+                ];
 
+const getCandidateSupporters = (connection) => (candidat) => new Promise((resolve, reject) => {
+    connection.query({
+        sql: 'SELECT * FROM `parrainages_enrichis` WHERE `candidat` = ?',
+        timeout: 36000, // 36.000s :-) 
+        values: candidat
+    }, function (error, results, fields) {
+        if (error) {
+            reject(error);
+            return;
+        }
+
+        resolve({
+            name: candidat,
+            parrainages: results,
+            total: results.length,
+        });
+    });
+ 
+});
+
+const saveResults = (result) =>  new Promise((resolve, reject) => {
+    const data = {};
+    result.forEach( function(element) {
+        data[element.name] = element;
+        
+    });
+
+    fs.writeFile('parrainages.json', JSON.stringify(data), (err) => { 
+        if (err) {
+            reject(err);
+            return;
+        }
+
+        console.log('File wrtitten');
+        resolve();
+    });
+});
 
 connection.connect(function(err) {
-	if (err) {
-		console.error('error connecting : ' + err.stack);
-		return;
-	}
+    if (err) {
+        console.error('error connecting : ' + err.stack);
+        return;
+    }
 
-	console.log('connected as id ' + connection.threadId);
+    console.log('connected as id ' + connection.threadId);
 
+    Promise.all(candidats.map(getCandidateSupporters(connection)))
+        .then(saveResults)
+        .catch(console.error)
+        .then(() => {
+            connection.end(function(err) {
+                console.log('The connection is terminated now');
+            });
+        });
 });
 
 
-candidats.forEach(function(candidat){
-
-	connection.query({
-
-  		sql: 'SELECT * FROM `parrainages_enrichis` WHERE `candidat` = ?',
-  		timeout: 3600, // 3.600s :-) 
-  		values: candidat
-	}, function (error, results, fields) {
-  		// error will be an Error if one occurred during the query 
-  		// results will contain the results of the query 
-  		// fields will contain information about the returned results fields (if any) 
-  		console.log('results : '+ JSON.stringify(results));
-	});
-});
+// {
 
 
-
-connection.end(function(err) {
-	console.log('The connection is terminated now');
-});
+// 'hfslqkdhfjk' : {
+//     name: 'àfsnqdkjlfh'
+// }
+// }
